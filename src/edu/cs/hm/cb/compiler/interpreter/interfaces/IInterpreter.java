66b255d066bb1e747fba797a9fcd7054d999130d
Manuel Schächinger <mtslive@me.com>
2012-12-10 22:59:21 +0100

    added Interpreter Interface

---
+++ src/edu/cs/hm/cb/compiler/interpreter/interfaces/IInterpreter.java
@@ -0,0 +1,14 @@
+//
+// IInterpreter.java
+// Compiler
+//
+// Created by Manuel Schaechinger on 04.12.2012
+// Copyrigtht (c) 2012 Delivery Studios. All rights reserved.
+//
+
+package edu.cs.hm.cb.compiler.interpreter.interfaces;
+
+public interface IInterpreter
+{
+
+}
